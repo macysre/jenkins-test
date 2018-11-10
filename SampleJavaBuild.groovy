eedc7a0f3081b14609abda8275fe220ca10a7fcc
Mani Marothu <Mani_Marothu@gap.com>
2018-11-10 01:43:30 -0800

    Added new groovy file

---
+++ SampleJavaBuild.groovy
@@ -0,0 +1,12 @@
+properties([pipelineTriggers([githubPush()])])
+
+node {
+    stage('checkout') {
+        git url: 'https://github.com/macysre/simple-java-maven-app.git', branch: 'master'
+    }
+
+    stage('Build') {
+        sh 'maven clean install'
+    }
+
+}
